e4dc9ba10f0074ce56e7559cfc82e2672001f2cc
PaulRen0519 <zhihe.24@intl.zju.edu.cn>
2025-03-14 11:16:37 +0800

    edited message

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -4,6 +4,7 @@ c = a + b
 d = 90
 e = 5
 f = d + e
+#judge whether c is bigger than f or not
 if c < f:
     print("Taking bus is faster.")
 elif c > f:
@@ -11,3 +12,16 @@ elif c > f:
 else:
     print("Taking bus and driving take the same time.")
 #Taking bus is faster.
+
+#add two variables x and y
+X = True 
+Y = False
+#creat a new variable called W
+W = X and Y
+
+#creat the truth table for W
+#X Y W
+#T T T
+#T F F
+#F T F
+#F F F
