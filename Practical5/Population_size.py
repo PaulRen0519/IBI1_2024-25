77fe3fc3a3d9f43c3302bdb2e1f94666008f4533
PaulRen0519 <zhihe.24@intl.zju.edu.cn>
2025-03-18 11:31:07 +0800

    Practical5

---
+++ Practical5/Population_size.py
@@ -0,0 +1,39 @@
+#store the population sizes for the UK and Zhejiang-neiphbouring provines
+uk_contries = [57.11, 3.13, 1.91, 5.45]
+zj_neibour_provines = [65.77, 41.88, 45.28, 61.27, 85.15]
+
+#sort the contries of the uk and provines of china
+sorted_uk = sorted(uk_contries)
+sorted_zj = sorted(zj_neibour_provines)
+
+#show the sorted population
+print("Sorted population of UK countries:", sorted_uk,
+      "\nSorted population of some Chinese provines:", sorted_zj)
+
+#import the package of drawing
+import matplotlib.pyplot as plt
+labels_uk = ['England', 'Wales', 'Northern Ireland', 'Scotland']
+explode = (0, 0, 0, 0.2)
+plt.pie(uk_contries, 
+        explode = explode, 
+        labels = labels_uk, 
+        autopct = '%1.1f%%', 
+        shadow = False, 
+        startangle = 90)
+
+#make sure the pie is a circle
+plt.axis('equal')
+plt.show()
+
+labels_ch = ['Zhejiang', 'Fujian', 'Jiangxi', 'Anhui', 'Jiangsu']
+explode = (0, 0, 0, 0.2, 0)
+plt.pie(zj_neibour_provines, 
+        explode = explode, 
+        labels = labels_ch, 
+        autopct = '%1.1f%%', 
+        shadow = True, 
+        startangle = 90)
+
+#make sure the pie is a circle
+plt.axis('equal')
+plt.show()
---
+++ Practical5/Programming_language_Popularity.py
@@ -0,0 +1,45 @@
+#make a dictionary of the language using 
+language_use = {
+    "JavaScript":62.3,
+    "HTML":52.9,
+    "Python":51,
+    "SQL":51,
+    "TypeScript":38.5
+    }
+
+#print the dictionary above
+print(language_use)
+
+#draw the plot describing the data
+#import the package numpy to calculate and matplotlib to draw graph
+import numpy as np
+import matplotlib.pyplot as plt
+
+#set variables of x and y axis
+languages = list(language_use.keys())
+percentages = list(language_use.values())
+
+#arange the average step length
+N = 5
+ind = np.arange(N)
+
+#set the width of the bar 
+width = 0.5
+
+#draw the graph
+barplot = plt.bar(languages, percentages, width)
+plt.ylabel('Persentages')
+plt.title('Programming Language Popularity')
+plt.xticks(ind, languages)
+plt.yticks(np.arange(0, 100, 10))
+
+#show the graph
+plt.show()
+
+#responde to the input language types and eliminate the upper or lower form of the input
+lower_languages = [lang.lower() for lang in languages]
+required_language = "Python"
+if required_language.lower() in lower_languages:
+    print(f"The percentage of {required_language} is {language_use[required_language]}%.")
+else:
+    print(f"Sorry, {required_language} is not included in the list.")
